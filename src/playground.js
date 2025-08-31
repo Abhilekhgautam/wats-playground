b8eb96ceae48bf89f99f9312aa5330d200043960
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2025-08-31 22:36:01 +0545

    fix: clear the terminal for every run

--- src/playground.js
+++ src/playground.js
@@ -48,6 +48,7 @@ function clearTerminal() {
 }
 
 function addLinesToTerminal(text) {
+  clearTerminal();
   if (text.startsWith("[")) {
     const lineElement = document.createElement("span");
     lineElement.innerHTML = ansiToHtml(text);
